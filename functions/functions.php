b041244cf9d70210482080db84f7214a3627dbab
impleCode <impleCode@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-06-01 07:49:23 +0000

    eCommerce Product Catalog 2.3.0
    git-svn-id: https://plugins.svn.wordpress.org/ecommerce-product-catalog/trunk@1171642 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- functions/functions.php
+++ functions/functions.php
@@ -523,7 +523,6 @@ function product_breadcrumbs() {
 		$post_type	 = get_post_type();
 		$home_page	 = get_home_url();
 		if ( function_exists( 'additional_product_listing_url' ) && $post_type != 'al_product' ) {
-			echo $post_type;
 			$catalog_id			 = catalog_id( $post_type );
 			$product_archives	 = additional_product_listing_url();
 			$product_archive	 = $product_archives[ $catalog_id ];
